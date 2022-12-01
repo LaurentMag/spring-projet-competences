ce164c74c50ae78c235fd4f14e02af4336040470
Laurent <magrez0laurent@gmail.com>
2022-12-01 09:56:55 +0100

    initial commit

---
+++ src/main/java/fr/moi/formation/projectcomp/springbootcompetenceproject/SpringBootCompetenceProjectApplication.java
@@ -0,0 +1,13 @@
+package fr.moi.formation.projectcomp.springbootcompetenceproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCompetenceProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootCompetenceProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fr/moi/formation/projectcomp/springbootcompetenceproject/SpringBootCompetenceProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.moi.formation.projectcomp.springbootcompetenceproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCompetenceProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
